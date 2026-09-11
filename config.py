6da7c3c02a903eaec26bf3ea71fe5f28752dace6
pathanraha56 <pathanraha56@gmail.com>
2026-09-11 11:17:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,35 +1,22 @@
+import os
 from flask_mysqldb import MySQL
 
 mysql = MySQL()
 
-
 class Config:
-
-    # ==========================================
-    # MYSQL DATABASE
-    # ==========================================
-
-    MYSQL_HOST = "localhost"
-
-    MYSQL_USER = "root"
-
-    MYSQL_PASSWORD = "your sql password"
-
-    MYSQL_DB = "smart_classroom_attendance"
-
-
-    # ==========================================
-    # GMAIL / FLASK-MAIL
-    # ==========================================
+    MYSQL_HOST = os.environ.get("MYSQL_HOST")
+    MYSQL_USER = os.environ.get("MYSQL_USER")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+    MYSQL_DB = os.environ.get("MYSQL_DB")
+    MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
 
     MAIL_SERVER = "smtp.gmail.com"
-
     MAIL_PORT = 587
-
     MAIL_USE_TLS = True
-
-    MAIL_USERNAME = "your email id"
-
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
     MAIL_PASSWORD = "16 digits password"
 
     MAIL_DEFAULT_SENDER = "your email id"
